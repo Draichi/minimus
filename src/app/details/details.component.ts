8d184c1a808cf3184682ad27c188a14c081b2348
draichi <lucasdraichi@gmail.com>
2020-03-30 21:17:01 -0300

    details component

---
+++ src/app/details/details.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.scss']
+})
+export class DetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
